f4928e931d8bbc923210331ea98001b3cf6afda5
Guilherme Oliveira <dongui04@hotmail.com>
2026-05-23 10:26:24 -0300

    feat: implementa autenticação no ChatWebSocketConfig e atualiza ChatWebSocketController para usar essa Authentication

--- src/main/java/com/nahora/controllers/ChatWebSocketController.java
+++ src/main/java/com/nahora/controllers/ChatWebSocketController.java
@@ -1,10 +1,12 @@
 package com.nahora.controllers;
 
 import com.nahora.dto.request.MensagemRequestDTO;
+import com.nahora.model.Usuario;
 import com.nahora.services.ChatService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 
 @Controller
@@ -14,6 +16,10 @@ public class ChatWebSocketController {
     private final ChatService chatService;
 
     @MessageMapping("/chat/{conversaId}/enviar")
-    public void enviarMensagem(@DestinationVariable Long conversaId, MensagemRequestDTO dto) {
+    public void enviarMensagem(@DestinationVariable Long conversaId, MensagemRequestDTO dto, Authentication authentication) {
+        // O Authentication vem preenchido pelo interceptador que criado no config do chatwebsocket, e o getPrincipal() retorna o usuário logado
+        Usuario remetente = (Usuario) authentication.getPrincipal();
+        
+        chatService.enviarMensagem(conversaId, remetente.getId(), dto);
     }
 }
